ad833b3145b2ec584abdb5550db6e6e56d0a8ca7
zohra-neda <zohra.neda@gmail.com>
2023-04-17 19:56:08 +0430

    Add script.js file

---
+++ script.js
@@ -0,0 +1,19 @@
+const hamburgerContainer = document.querySelector('.hamburger-container');
+const hamburgerItems = document.querySelectorAll('.menuLink');
+const closeHamburger = document.querySelector('.hamburger-close');
+const openHamburger = document.querySelector('#hamburger');
+
+function hamburgerShow() {
+  hamburgerContainer.setAttribute('style', 'display: block');
+}
+
+function hamburgerHide() {
+  hamburgerContainer.setAttribute('style', 'display: none');
+}
+
+openHamburger.addEventListener('click', hamburgerShow);
+closeHamburger.addEventListener('click', hamburgerHide);
+
+hamburgerItems.forEach((item) => {
+    item.addEventListener('click', hamburgerHide);
+  });
