35735b275356addb19429f49a92c0fed0b370c5a
zohra-neda <zohra.neda@gmail.com>
2023-04-19 19:58:20 +0430

    Update projects array

--- script.js
+++ script.js
@@ -22,45 +22,22 @@ hamburgerItems.forEach((item) => {
 
 const projectContainer = document.querySelector('.project-container');
 
-const workSectionProject = {
-  id: 1,
-  image: 'Img Placeholder.svg',
-  title: 'Multi-Post Stories',
-  paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-  lang1: 'CSS',
-  lang2: 'HTML',
-  lang3: 'Bootstrap',
-  lang4: 'Ruby',
-  button: 'See Project',
-};
-
-function workSectionProjectFunction() {
-  const projectDetails = document.createElement('div');
-  projectDetails.classList.add('project-details');
-  projectDetails.innerHTML = `
-    <h2 class="project-item second-tittle">${workSectionProject.title}</h2>
-    <p class="project-item project-p">${workSectionProject.paragraph}</p>
-    <ul class="project-item lang-buttons-container">
-      <li class="lang-list lang-css"><a class="lang-list-link" href="#">${workSectionProject.lang1}</a></li>
-      <li class="lang-list lang-html"><a class="lang-list-link" href="#">${workSectionProject.lang2}</a></li>
-      <li class="lang-list lang-boot"><a class="lang-list-link" href="#">${workSectionProject.lang3}</a></li>
-      <li class="lang-list lang-ruby"><a class="lang-list-link" href="#">${workSectionProject.lang4}</a></li>
-    </ul>
-    <button class="project-item buttons project-button projects-btn" onClick='popupShow();' id="pro-btn" type="button">${workSectionProject.button}</button>`;
-
-  projectContainer.innerHTML = `
-  <img class="project-item project-image" src="./images/mobile-images/section-1/${workSectionProject.image}" alt="project-image">`;
-
-  projectContainer.appendChild(projectDetails);
-}
-
-window.addEventListener('load', workSectionProjectFunction);
-
 // CART SECTION START
 
 const cardContainer = document.querySelector('.cart-container');
 
 const cardSectionProject = [
+  {
+    id: 1,
+    image: 'Img Placeholder.svg',
+    title: 'Multi-Post Stories',
+    paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    lang1: 'CSS',
+    lang2: 'HTML',
+    lang3: 'Bootstrap',
+    lang4: 'Ruby',
+    button: 'See Project',
+  },
   {
     cardNumber: 1,
     cardTitle: 'Professional Art Printing Data',
@@ -98,23 +75,42 @@ function cardSectionProjectFunction() {
   const cardRowTwo = document.createElement('div');
   cardRowTwo.classList.add('cart-row2');
 
-  cardSectionProject.forEach((Project) => {
-    [cardRowOne, cardRowTwo].forEach((row) => {
-      row.innerHTML += `
-        <div class="cart-project-container hide-text">
-          <h2 class="cart-project-item cart-title">${Project.cardTitle}</h2>
-          <p class="cart-project-item cart-p">${Project.cardParagraph}</p>
-          <ul class="card-project-item cart-lang-container">
-            <li class="cart-lang-item cart-lang-html"><a class="cart-lang-link" href="#">${Project.lang1}</a></li>
-            <li class="cart-lang-item cart-lang-boot"><a class="cart-lang-link" href="#">${Project.lang2}</a></li>
-            <li class="cart-lang-item cart-lang-ruby"><a class="cart-lang-link" href="#">${Project.lang3}</a></li>
-          </ul>
-          <button class="cart-project-item project-btn buttons projects-btn" type="button" onClick='popupShow();'>${Project.button}</button>
-        </div>`;
-    });
+  cardSectionProject.forEach((Project, index) => {
+    if (index === 0) {
+      const projectDetails = document.createElement('div');
+      projectDetails.classList.add('project-details');
+      projectDetails.innerHTML = `
+        <h2 class="project-item second-tittle">${Project.title}</h2>
+        <p class="project-item project-p">${Project.paragraph}</p>
+        <ul class="project-item lang-buttons-container">
+          <li class="lang-list lang-css"><a class="lang-list-link" href="#">${Project.lang1}</a></li>
+          <li class="lang-list lang-html"><a class="lang-list-link" href="#">${Project.lang2}</a></li>
+          <li class="lang-list lang-boot"><a class="lang-list-link" href="#">${Project.lang3}</a></li>
+          <li class="lang-list lang-ruby"><a class="lang-list-link" href="#">${Project.lang4}</a></li>
+        </ul>
+        <button class="project-item buttons project-button projects-btn" onClick='popupShow();' id="pro-btn" type="button">${Project.button}</button>`;
 
-    cardContainer.appendChild(cardRowOne);
-    cardContainer.appendChild(cardRowTwo);
+      projectContainer.innerHTML = `
+      <img class="project-item project-image" src="./images/mobile-images/section-1/${Project.image}" alt="project-image">`;
+
+      projectContainer.appendChild(projectDetails);
+    } else {
+      [cardRowOne, cardRowTwo].forEach((row) => {
+        row.innerHTML += `
+          <div class="cart-project-container hide-text">
+            <h2 class="cart-project-item cart-title">${Project.cardTitle}</h2>
+            <p class="cart-project-item cart-p">${Project.cardParagraph}</p>
+            <ul class="card-project-item cart-lang-container">
+              <li class="cart-lang-item cart-lang-html"><a class="cart-lang-link" href="#">${Project.lang1}</a></li>
+              <li class="cart-lang-item cart-lang-boot"><a class="cart-lang-link" href="#">${Project.lang2}</a></li>
+              <li class="cart-lang-item cart-lang-ruby"><a class="cart-lang-link" href="#">${Project.lang3}</a></li>
+            </ul>
+            <button class="cart-project-item project-btn buttons projects-btn" type="button" onClick='popupShow();'>${Project.button}</button>
+          </div>`;
+      });
+      cardContainer.appendChild(cardRowOne);
+      cardContainer.appendChild(cardRowTwo);
+    }
   });
 }
 // T E
