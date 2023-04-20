46cdc31724b0cf0de58d1e31c7f58e80f2258560
zohra-neda <zohra.neda@gmail.com>
2023-04-20 18:54:47 +0430

    Update script.js file

--- script.js
+++ script.js
@@ -199,7 +199,7 @@ textArea.addEventListener('input', saveFormData);
 form.addEventListener('submit', (e) => {
   if (!emailRegExp.test(email.value)) {
     e.preventDefault();
-    error.textContent = 'Expected email address to be lowercase!';
+    error.textContent = 'Expected valid email address!';
   } else {
     error.textContent = '';
   }
