253e03cf58d6604f0a2e2422143f92abb620299a
zohra-neda <zohra.neda@gmail.com>
2023-11-21 15:57:13 +0430

    Update modal.js file

--- js/modal.js
+++ js/modal.js
@@ -7,7 +7,7 @@ const works = [
     live: 'https://drive-share-app-frontend.onrender.com/',
     repository: 'https://github.com/Zohra-Neda/DriveShare-Back-End',
     technology: ['Ruby on Rails', 'React JS', 'Redux', 'Postgresql', 'Tailwind CSS'],
-    description: `DriveShare is a comprehensive full-stack web application designed to streamline the processes of adding, reserving, and deleting cars.`,
+    description: 'DriveShare is a comprehensive full-stack web application designed to streamline the processes of adding, reserving, and deleting cars.',
   },
   {
     name: 'Metrics Web App',
@@ -17,7 +17,7 @@ const works = [
     live: 'https://metrics-web-app-zn.onrender.com/',
     repository: 'https://github.com/Zohra-Neda/Metrics-Web-App',
     technology: ['React', 'Redux', 'HTML5', 'CSS3'],
-    description: `This is a React application that displays information about various cryptocurrencies using data from the CoinStats API (https://api.coinstats.app/public/v1/coins). The app allows users to search for specific cryptocurrencies, view detailed information about each currency, and filter currencies by price, market cap, and other metrics.`,
+    description: 'This is a React application that displays information about various cryptocurrencies using data from the CoinStats API (https://api.coinstats.app/public/v1/coins). The app allows users to search for specific cryptocurrencies, view detailed information about each currency, and filter currencies by price, market cap, and other metrics.',
   },
   {
     name: "Space-Travelers' Hub",
@@ -28,7 +28,7 @@ const works = [
     repository: 'https://github.com/Zohra-Neda/Space-Travelers-Hub',
     technology: ['HTML', 'CSS', 'React', 'Redux'],
     description:
-      `The Space Traveler Hub is an app built with React and Redux. On the main page, it displays rockets along with their detailed explanations. The mission page allows users to view available missions. Users are also allowed to reserve missions or join meetings. In the "My Profile" page, users can see the missions they have joined and the rockets they have reserved.`,
+      'The Space Traveler Hub is an app built with React and Redux. On the main page, it displays rockets along with their detailed explanations. The mission page allows users to view available missions. Users are also allowed to reserve missions or join meetings. In the "My Profile" page, users can see the missions they have joined and the rockets they have reserved.',
   },
   {
     name: 'Awesome Movies',
@@ -39,7 +39,7 @@ const works = [
     repository: 'https://github.com/Zohra-Neda/JavaScript-Capstone-Project',
     technology: ['HTML', 'CSS', 'JavaScript'],
     description:
-      `Welcome to our open-source website repository on GitHub! this website utilizes APIs to provide an interactive platform for visitors to engage with the content and share their thoughts.`,
+      'Welcome to our open-source website repository on GitHub! this website utilizes APIs to provide an interactive platform for visitors to engage with the content and share their thoughts.',
   },
 
   {
@@ -50,7 +50,7 @@ const works = [
     repository: 'https://github.com/Zohra-Neda/Awesome-Books',
     technology: ['HTML', 'CSS', 'JavaScript'],
     description:
-      ` is a mini website which is created by html css and javascript, looking for a fun and interactive way to organize your book collection? Look no further than Awesome Book! This mini website is the perfect solution for book lovers who want to keep track of their favorite reads in a visually appealing and user-friendly way.`,
+      'This is a mini website which is created by html css and javascript, looking for a fun and interactive way to organize your book collection? Look no further than Awesome Book! This mini website is the perfect solution for book lovers who want to keep track of their favorite reads in a visually appealing and user-friendly way.',
   },
   {
     name: 'To-Do List',
@@ -60,7 +60,7 @@ const works = [
     repository: 'https://github.com/Zohra-Neda/To-Do-List',
     technology: ['HTML5', 'CSS3', 'JavaScript(ES6)', 'Webpack'],
     description:
-      `is a mini app in which the user can add, edit, complete or delete tasks. And user can also clear all the completed tasks.`,
+      'is a mini app in which the user can add, edit, complete or delete tasks. And user can also clear all the completed tasks.',
   },
 ];
 
@@ -88,8 +88,8 @@ function projectsAll() {
      </div>
      <ul class="bags d-flex list-none">
        ${work.technology
-         .map((item) => `<li class="tags-li">${item}</li>`)
-         .join('')}  
+    .map((item) => `<li class="tags-li">${item}</li>`)
+    .join('')}
      </ul>
      <div class="action">
          <button type="button" class="btn project button_project-1" data-tab="1">See Project</button>
@@ -116,11 +116,11 @@ function modalWindow() {
   <li>${work.tags[2]}</li>
   </ul>
   </div>
-  
+
   <img class="sample mod" src='${
-    work.image
-  }' alt="multi-purpose stories project snapshoot">
-  
+  work.image
+}' alt="multi-purpose stories project snapshoot">
+
   <header class="project-header">
   <p class="project-header-p">${work.description}</p>
 
@@ -131,11 +131,11 @@ function modalWindow() {
   <div class="rule"></div>
   <div class="btnflex">
   <a target="_blank" href="${
-    work.live
-  }" class="btn mod" type="submit">see live<img class="btn-icon" src="./images/Icon.png" alt="check live version"></a>
+  work.live
+}" class="btn mod" type="submit">see live<img class="btn-icon" src="./images/Icon.png" alt="check live version"></a>
   <a target="_blank" href="${
-    work.repository
-  }" class="btn mod" type="submit">see source<img class="btn-icon" src="./images/github.png" alt="check live version"></a
+  work.repository
+}" class="btn mod" type="submit">see source<img class="btn-icon" src="./images/github.png" alt="check live version"></a
   </div>
   </div>
   
